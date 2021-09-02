4ed0e1ec80a01134735f3201583c01d2fcaf9ff7
osmktok <o_k.2o11@icloud.com>
2021-09-02 12:18:21 +0900

    Userのモデル単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字、ひらがな、カタカナ）を使用してください' } do
     validates :family_name
     validates :first_name
   end
