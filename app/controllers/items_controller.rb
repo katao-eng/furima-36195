d26fe118f4820715a8859fa59062f3e5a5952405
osmktok <o_k.2o11@icloud.com>
2021-09-03 14:06:29 +0900

    ビューファイルの設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,15 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end
