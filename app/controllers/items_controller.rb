0abff79dac52e05d1fbf537222484152426b1064
osmktok <o_k.2o11@icloud.com>
2021-09-03 15:42:25 +0900

    ActiveHashの導入とビューファイルの訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :defrayer_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :defrayer
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
   validates :name, presence: true
   validates :explanation, presence: true
@@ -11,4 +17,6 @@ class Item < ApplicationRecord
   validates :delivery_day_id, presence: true
   validates :price, presence: true
   validates :image, presence: true
+
+  validates :category_id, :condition_id, :defrayer_id, :prefecture_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
