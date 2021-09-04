7dd6f99e651804f9915a9d49c6a397a179ca6769
osmktok <o_k.2o11@icloud.com>
2021-09-04 14:09:57 +0900

    Itemのモデル単体テストの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name            { Faker::Lorem.sentence }
+    explanation     { Faker::Lorem.sentence }
+    category_id     { 2 }
+    condition_id    { 2 }
+    defrayer_id     { 2 }
+    prefecture_id   { 2 }
+    delivery_day_id { 2 }
+    price           { 19800 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
