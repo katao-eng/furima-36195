3522a466958591488a353944e9f1dfa72c61318f
osmktok <o_k.2o11@icloud.com>
2021-09-05 19:23:28 +0900

    ログイン状態のみ削除可能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
@@ -35,7 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
